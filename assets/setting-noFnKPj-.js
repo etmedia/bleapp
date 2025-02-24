78d0ecf2670e05aa249cf33fa715ed665d6c1aef
etmedia <etmedia@qq.com>
2025-02-24 06:09:23 +0000

    Deploying to gh-pages from @ etmedia/bleapp@54e53455a6f6daae044b278c7bd5bc99a25e4ec1 🚀

---
+++ assets/setting-noFnKPj-.js
@@ -0,0 +1,1 @@
+import{d as t,c as s,a as o,o as n,_ as a}from"./index-D6h8mZxo.js";const _={class:"setting"},c=t({__name:"setting",setup(r){return(p,e)=>(n(),s("div",_,e[0]||(e[0]=[o("h3",null,"设置参数",-1)])))}}),i=a(c,[["__scopeId","data-v-2c823560"]]);export{i as default};
